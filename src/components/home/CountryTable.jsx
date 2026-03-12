a48f4a35416bd9c7bd6f066955c99e93d287fed7
Or Nachshon <or.nachshon@gmail.com>
2026-03-12 20:10:35 +0000

    File changes

--- src/components/home/CountryTable.jsx
+++ src/components/home/CountryTable.jsx
@@ -1,37 +1,5 @@
 import React from "react";
-
-const countries = [
-  { rank: 1,  name: "Singapore",     iq: 108 },
-  { rank: 2,  name: "Hong Kong",      iq: 108 },
-  { rank: 3,  name: "Taiwan",         iq: 106 },
-  { rank: 4,  name: "South Korea",    iq: 106 },
-  { rank: 5,  name: "Japan",          iq: 105 },
-  { rank: 6,  name: "China",          iq: 104 },
-  { rank: 7,  name: "Slovenia",       iq: 103 },
-  { rank: 8,  name: "Switzerland",    iq: 102 },
-  { rank: 9,  name: "The Netherlands",iq: 102 },
-  { rank: 10, name: "North Korea",    iq: 102 },
-  { rank: 11, name: "Macao",          iq: 101 },
-  { rank: 12, name: "Iceland",        iq: 101 },
-  { rank: 13, name: "Finland",        iq: 101 },
-  { rank: 14, name: "Canada",         iq: 101 },
-  { rank: 15, name: "Belgium",        iq: 100 },
-  { rank: 16, name: "Germany",        iq: 100 },
-  { rank: 17, name: "United Kingdom", iq: 100 },
-  { rank: 18, name: "Austria",        iq: 100 },
-  { rank: 19, name: "New Zealand",    iq: 100 },
-  { rank: 20, name: "Israel",         iq: 100 },
-  { rank: 21, name: "Malta",          iq: 99  },
-  { rank: 22, name: "Slovakia",       iq: 99  },
-  { rank: 23, name: "Norway",         iq: 99  },
-  { rank: 24, name: "Sweden",         iq: 99  },
-  { rank: 25, name: "Luxembourg",     iq: 99  },
-  { rank: 26, name: "Denmark",        iq: 99  },
-  { rank: 27, name: "Czech Republic", iq: 99  },
-  { rank: 28, name: "Estonia",        iq: 99  },
-  { rank: 29, name: "Australia",      iq: 99  },
-  { rank: 30, name: "France",         iq: 98  },
-];
+import { countries } from "./countryData";
 
 const cols = [countries.slice(0, 10), countries.slice(10, 20), countries.slice(20, 30)];
 
