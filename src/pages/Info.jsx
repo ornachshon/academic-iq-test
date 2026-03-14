7ae15c9dbf46d066bbdca40fd323c9bef615809b
Or Nachshon <or.nachshon@gmail.com>
2026-03-14 13:21:38 +0000

    File changes

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import { format } from "date-fns";
 
 export default function Info() {
